b1e16c508f45b33fd39dbc1da9b62288c7a1d76b
Sourav-Kumar-Nayak <souravkumarnayak041@gmail.com>
2025-07-28 19:49:11 +0530

    add updated user-module

---
+++ User/src/main/java/com/AgroLink/User/enums/UserRole.java
@@ -0,0 +1,13 @@
+package com.AgroLink.User.enums;
+
+public enum UserRole {
+    ADMIN,
+    MANAGER,
+    CUSTOMER,
+    FARMER,
+    BUYER,
+    CARRIER,
+    WAREHOUSE_OPERATOR,
+    ADVISOR,
+    SUSPENDED_USER;
+}
---
+++ User/src/test/java/com/AgroLink/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgroLink.User;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
