d1fea2a2d3b4a15a57dcfdbfdf1ce8965d2b118e
Sourav-Kumar-Nayak <souravkumarnayak041@gmail.com>
2025-08-02 00:40:20 +0530

    chore: update Eureka Client configuration and add ServiceRegistry module

--- AgroLink_Backend/order/src/main/java/com/AgroLink/order/OrderApplication.java
+++ AgroLink_Backend/order/src/main/java/com/AgroLink/order/OrderApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
