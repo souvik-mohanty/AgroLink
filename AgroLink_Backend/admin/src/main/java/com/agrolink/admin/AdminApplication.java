b3f704e2a1b285b140c114d409c6080d3b81060d
Sourav-Kumar-Nayak <souravkumarnayak041@gmail.com>
2025-08-01 22:31:33 +0530

    chore: update PostgreSQL configuration and remove Eureka Client dependencies

--- AgroLink_Backend/admin/src/main/java/com/agrolink/admin/AdminApplication.java
+++ AgroLink_Backend/admin/src/main/java/com/agrolink/admin/AdminApplication.java
@@ -2,9 +2,8 @@ package com.agrolink.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class AdminApplication {
 
--- AgroLink_Backend/fixed_advisory/src/main/java/com/agrolink/advisory/AdvisoryApplication.java
+++ AgroLink_Backend/fixed_advisory/src/main/java/com/agrolink/advisory/AdvisoryApplication.java
@@ -2,9 +2,8 @@ package com.agrolink.advisory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class AdvisoryApplication {
 
